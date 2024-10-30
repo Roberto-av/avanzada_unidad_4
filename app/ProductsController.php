71552076553e002d18d891ec64e19c29f61e6b5c
Roberto Aviles <avilesrobertoe@gmail.com>
2024-10-29 18:38:20 -0700

    funcion para actualizar un producto

--- app/ProductsController.php
+++ app/ProductsController.php
@@ -102,4 +102,35 @@ class ProductsController
 			error_log("error");
 		}
 	}
+
+	public function updateProduct($nombre,$slug,$description,$features,$id)
+	{
+		$curl = curl_init();
+
+		curl_setopt_array($curl, array(
+		  CURLOPT_URL => 'https://crud.jonathansoto.mx/api/products',
+		  CURLOPT_RETURNTRANSFER => true,
+		  CURLOPT_ENCODING => '',
+		  CURLOPT_MAXREDIRS => 10,
+		  CURLOPT_TIMEOUT => 0,
+		  CURLOPT_FOLLOWLOCATION => true,
+		  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		  CURLOPT_CUSTOMREQUEST => 'PUT',
+		  CURLOPT_POSTFIELDS => 'name='. $nombre. '&slug=' .$slug. '&description='.$description. '&features=' .$features. '&id='.$id ,
+		  CURLOPT_HTTPHEADER => array(
+			'Content-Type: application/x-www-form-urlencoded',
+			'Authorization: Bearer ' . $_SESSION['user_data']->token
+		  ),
+		));
+		
+		$response = curl_exec($curl);
+		curl_close($curl);
+		$response = json_decode($response);
+		
+		if (isset($response->data)  && is_object($response->data)) {
+			header("Location: ../avanzada_unidad_4/home.php");
+		} else {
+			error_log("error");
+		}
+	}
 }
